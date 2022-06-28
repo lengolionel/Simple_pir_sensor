0a777f73b8dc02b80e5e2cf2d29179748d39810e
idukunda lengo lionel <36621498+lengolionel@users.noreply.github.com>
2022-06-28 21:08:41 +0200

    Add files via upload

---
+++ sketch_jun28a.ino
@@ -0,0 +1,24 @@
+int inputPin= 2;
+int pirState = LOW;
+int val=0;
+void setup() {
+  pinMode(inputPin,INPUT);
+  Serial.begin(9600);
+  }
+
+void loop() {
+  val = digitalRead(inputPin);
+
+  if(val==HIGH){
+    if(pirState==LOW){
+      Serial.println("Motion detected");
+      pirState=HIGH;
+      }
+    }
+    else{
+      if(pirState==HIGH){
+        Serial.println("Motion Ended!");
+        pinState=LOW;
+        }
+      }
+  }
